9ce7b921aef9ca52e463f63e965484df398ff85c
naimmanriquez <66890535+naimmanriquez@users.noreply.github.com>
2023-02-08 11:49:46 -0600

    Add files via upload

---
+++ 02_codigos/instalar_librerias.R
@@ -0,0 +1,47 @@
+## Instalar librerias
+
+## Herramientas para manipulacion de datos
+install.packages("dplyr")
+install.packages("tidyverse")
+install.packages("knitr")
+install.packages("DescTools")
+install.packages("expss")
+
+
+## Herramientas para mapas y analisis espacial
+install.packages("sf")
+install.packages("ggspatial")
+install.packages("tmap")
+install.packages("spdep")
+install.packages("rgdal")
+
+
+## Herramientas para visualizacion y graficas
+install.packages("ggplot2")
+install.packages("viridis")
+install.packages("ggthemes")
+install.packages("RColorBrewer")
+
+
+## Herramientas de estadistica
+install.packages("lmtest")
+install.packages("gmodels")
+install.packages("MASS")
+
+
+## Otras herramientas y para importar archivos de otros formatos
+install.packages("devtools")
+install.packages("readxl")
+install.packages("haven")
+install.packages("leaflet")
+install.packages("gtable")
+install.packages("viridisLite")
+install.packages("rlang")
+install.packages("data.table")
+install.packages("viridisLite")
+install.packages("broom")
+install.packages("cellranger")
+
+
+
+
